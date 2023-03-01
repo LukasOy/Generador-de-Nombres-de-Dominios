f01a5690fa89e8c7c67d868439f496ea4f97063a
LukasOy <lukas.oylu@gmail.com>
2023-03-01 17:27:57 +0000

    generadorlisto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("#GND").innerHTML = NombreDominio();
+
+function NombreDominio() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let P = 0;
+  let A = 0;
+  let N = 0;
+
+  let i = "";
+
+  for (let P = 0; P < pronoun.length; P++) {
+    for (let A = 0; A < adj.length; A++) {
+      for (let N = 0; N < noun.length; N++) {
+        i += pronoun[P] + adj[A] + noun[N] + ".com <br>";
+      }
+    }
+  }
+  return i;
+}
